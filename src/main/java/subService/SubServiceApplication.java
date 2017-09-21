988a3d1952ce3a44dd97e95c42846b67c425f6f8
Mayank Srivastava <mayankdiatm@gmail.com>
2017-09-21 10:48:02 +0000

    Fresh Commit

---
+++ src/main/java/subService/SubServiceApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubServiceApplication.class, args);
+	}
+}
